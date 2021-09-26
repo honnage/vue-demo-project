e955867e95cd03bc9453721747e4d082847eb850
unknown <hon_da-1998@hotmail.com>
2021-09-26 23:55:17 +0700

    Interpolation and Click Event Listener

--- vue_demo_1/src/App.vue
+++ vue_demo_1/src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <!-- interpolation -->
+    <h1>{{ title }}</h1>
+    <h2>{{count}}</h2>
+    <button @click="onClickAdd">Add</button>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Test www.test.com",
+      count:0
+    };
+  },
+  methods: {
+    onClickAdd() {
+      this.count= this.count + 1
+    }
+  },
+};
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
