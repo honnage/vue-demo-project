2247c51563554f28f4c115c4767a8eb635442e3f
Hon Passakorn <47750693+honnage@users.noreply.github.com>
2021-10-05 14:37:22 +0700

    Dictionary and Array Data

--- vue_demo_1/src/views/Demo3_Data.vue
+++ vue_demo_1/src/views/Demo3_Data.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <h1>Data</h1>
-    <button @click="count++; isLogin=true">Count++</button>
+    <button @click="count++; isLogin = true;">
+        Count++
+    </button>
     <div>{{ count }}</div>
     <div>{{ message }}</div>
     <div>{{ isLogin }}</div>
-    <input type="text" placeholder="Enter a message" v-model="message">
-    <h2 v-if="message == 'hello' || isLogin == true ">Yahoo</h2>
+    <input type="text" placeholder="Enter a message" v-model="message" />
+    <h2 v-if="message == 'hello' || isLogin == true">Yahoo</h2>
+    <div>{{account}}</div>
+    <div>{{courses}}</div>
+    <ul>
+        <li v-for="(item, index) in courses" :key="index">{{index + 1}}.{{item}}</li>
+    </ul>
   </div>
 </template>
 
@@ -20,10 +27,25 @@ export default {
     return {
       count: 0,
       message: "",
-      isLogin: false
+      isLogin: false,
+      account: {
+        username: "admin",
+        password: "1234"
+      },
+      courses:[
+        "Angular",
+        "VueJs",
+        "ReactJS",
+        "Fluter",
+        "UXUI"
+      ]
     };
   }
 };
 </script>
 
-<style></style>
+<style scoped>
+    li{
+        color: red;
+    }
+</style>
