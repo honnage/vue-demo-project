fa29e699a21880308e68a2b0b7bad17d53a9c434
Hon Passakorn <47750693+honnage@users.noreply.github.com>
2021-10-06 14:44:29 +0700

    Modifier Number, Trim, Lazy Shortkey

--- vue_demo_1/src/views/Demo5_Form.vue
+++ vue_demo_1/src/views/Demo5_Form.vue
@@ -3,20 +3,33 @@
     <form v-on:submit.prevent="submit">
       <div>
         <label for="first_name">First Name</label>
-        <input type="text" name="first_name" v-model="applicant.first_name" />
+        <input
+          type="text"
+          name="first_name"
+          v-model.trim="applicant.first_name"
+        />
       </div>
 
       <div>
         <label for="first_name">Last Name</label>
-        <input type="text" name="last_name" v-model="applicant.last_name" />
+        <input
+          type="text"
+          name="last_name"
+          v-model.trim="applicant.last_name"
+        />
       </div>
 
       <div>
-        <label for="sex">Last Name</label>
+        <label for="sex">Male</label>
         <input type="radio" name="sex" value="male" v-model="applicant.sex" />
 
-        <label for="sex">Last Name</label>
+        <label for="sex">Female</label>
         <input type="radio" name="sex" value="female" v-model="applicant.sex" />
+      </div>
+
+      <div>
+        <label for="age">Age</label>
+        <input type="number" v-model.number="applicant.age" />
       </div>
 
       <div>
@@ -28,7 +41,7 @@
       </div>
 
       <!-- <button type="button" @click="applicant = {}">Clear</button> -->
-      <button type="button" @click="clear">Clear</button>
+      <button type="button" @click.exact="clear" @click.alt="useDefault">Clear</button>
       <button type="submit">Submit</button>
     </form>
 
@@ -47,21 +60,32 @@ export default {
         first_name: "",
         last_name: "",
         sex: "male",
-        courses: "react"
+        courses: "react",
+        age: 0
       }
     };
   },
   methods: {
     submit() {
-        // event.preventDefault();
-        alert(JSON.stringify(this.applicant))
+      // event.preventDefault();
+      alert(JSON.stringify(this.applicant));
     },
     clear() {
       this.applicant = {
         first_name: "",
         last_name: "",
         sex: "male",
-        courses: "react"
+        courses: "react",
+        age: 0
+      };
+    },
+    useDefault() {
+      this.applicant = {
+        first_name: "UNKNOWN",
+        last_name: "UNKNOWN",
+        sex: "male",
+        courses: "react",
+        age: 10
       };
     }
   }
