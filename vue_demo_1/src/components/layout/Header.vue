ddd532b4d9eef39625b73c3a403f6e53a267de46
Hon Passakorn <47750693+honnage@users.noreply.github.com>
2021-10-06 15:03:50 +0700

    Create and Call Sub Components

---
+++ vue_demo_1/src/components/layout/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul>
+      <li><router-link to="/"><button>Home</button></router-link></li>
+      <li><router-link to="/demo1"><button> Demo1 Intro </button></router-link></li>
+      <li><router-link to="/demo2"><button> Demo2 Events </button></router-link></li>
+      <li><router-link to="/demo3"><button> Demo3 Data </button></router-link></li>
+      <li><router-link to="/demo4"><button> Demo4 Binding </button></router-link></li>
+      <li><router-link to="/demo5"><button> Demo5 Form & v-model </button></router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+ul {
+  margin: 0;
+  padding: 0;
+}
+li {
+  display: inline;
+  margin-right: 10px;
+}
+
+.title {
+  font-size: 30px;
+  color: green;
+}
+</style>
+
+<style>
+.content {
+  color: violet;
+}
+</style>
