37c9fe75d2018641717e56bdc31a1ad4d0a94359
honnage <hon_da-1998@hotmail.com>
2022-02-20 14:14:47 +0700

    vuex

---
+++ vue_demo_1/src/components/Popup.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-dialog max-width="600px">
+        <v-btn flat slot="activator" class="success">addnew</v-btn>
+        <v-card-title>
+            <h2>new Project</h2>
+        </v-card-title>
+        
+    </v-dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- vue_demo_1/src/views/Home.vue
+++ vue_demo_1/src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Home</h1>
+    <v-flex class="mt-4">
+      <Popup />
+    </v-flex>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
@@ -9,10 +12,11 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
+import Popup from '../components/Popup.vue'
 export default {
   name: 'Home',
   components: {
+    Popup,
     // HelloWorld
   }
 }
