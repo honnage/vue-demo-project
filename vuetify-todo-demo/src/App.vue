fddc9f76d162cd8b97081cb6fc45717f680098ef
honnage <hon_da-1998@hotmail.com>
2022-02-21 12:13:42 +0700

    TODO Worksshop

---
+++ vuetify-todo-demo/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+       <router-link to="/">
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+       </router-link>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+       @click="$router.push('/about')"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">About us</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-content>
+        <router-view />
+      </v-content>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
