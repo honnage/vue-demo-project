46e747559c0771c273ea5f5f6ec975b5a536ba96
honnage <hon_da-1998@hotmail.com>
2022-02-21 12:24:57 +0700

    v-for

--- vuetify-todo-demo/src/views/Home.vue
+++ vuetify-todo-demo/src/views/Home.vue
@@ -1,15 +1,33 @@
 <template>
-  <hello-world />
+  <div class="home">
+    <v-container>
+      <v-row
+        class="d-flex flex-row align-center"
+        v-for="(item, index) in todos"
+        :key="index"
+      >
+        <v-checkbox v-model="value" value="value"></v-checkbox>
+        <span>Title</span>
+        <v-spacer> </v-spacer>
+        <v-btn color="error">x</v-btn>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
   name: "Home",
 
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Task 1", completed: false },
+        { id: 2, title: "Task 2", completed: false },
+        { id: 3, title: "Task 3", completed: false },
+      ],
+    };
   },
 };
 </script>
