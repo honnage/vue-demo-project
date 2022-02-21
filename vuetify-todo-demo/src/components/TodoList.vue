f1015e59bda3f63afd142175932440e8268836ba
honnage <hon_da-1998@hotmail.com>
2022-02-21 12:55:36 +0700

    Sub Component & Props Data Sharing

---
+++ vuetify-todo-demo/src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card>
+    <v-card-title primary-title>
+      <v-container>
+        <v-row
+          class="d-flex flex-row align-center"
+          v-for="(item, index) in todos"
+          :key="index"
+        >
+          <v-col cols="12" class="d-flex flex-row align-center pa-0">
+            <v-checkbox
+              class="mr-10"
+              v-model="value"
+              value="value"
+            ></v-checkbox>
+            <span>{{ item.title }}</span>
+            <v-spacer> </v-spacer>
+            <v-btn color="error">x</v-btn>
+          </v-col>
+          <v-col col="12">
+            <v-divider></v-divider>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TodoList ",
+  props:["todos"]
+};
+</script>
+
+<style></style>
--- vuetify-todo-demo/src/views/Home.vue
+++ vuetify-todo-demo/src/views/Home.vue
@@ -11,36 +11,21 @@
         </v-col>
       </v-row>
 
-      <v-card>
-        <v-card-title primary-title>
-          <v-container>
-            <v-row
-              class="d-flex flex-row align-center"
-              v-for="(item, index) in todos"
-              :key="index"
-            >
-              <v-col cols="12" class="d-flex flex-row align-center">
-                <v-checkbox v-model="value" value="value"></v-checkbox>
-                <span>{{item.title}}</span>
-                <v-spacer> </v-spacer>
-                <v-btn color="error">x</v-btn>
-              </v-col>
-              <v-col col="12">
-                <v-divider></v-divider>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-title>
-      </v-card>
+      <TodoList :todos="todos" />
+
+      
     </v-container>
   </div>
 </template>
 
 <script>
+import TodoList from "@/components/TodoList.vue"
 export default {
   name: "Home",
 
-  components: {},
+  components: {
+    TodoList
+  },
   methods: {
     addTask(){
       this.todos.push({
